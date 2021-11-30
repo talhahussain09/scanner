1cd4e210f73dbc921a3b1b2ec9e6bfc4edade2a3
talhahussain09 <49669460+talhahussain09@users.noreply.github.com>
2021-11-30 14:29:07 +0500

    Update example-scanner.py

--- example-scanner.py
+++ example-scanner.py
@@ -17,3 +17,5 @@ for dev in devices:
     print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
     for (adtype, desc, value) in dev.getScanData():
         print("  %s = %s" % (desc, value))
+while True:
+    scanner.process()

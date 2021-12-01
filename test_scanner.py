27e4965d0581657d0dbb2a1bb977d493388e66f6
talhahussain09 <49669460+talhahussain09@users.noreply.github.com>
2021-12-01 10:04:14 +0500

    Add files via upload

--- test_scanner.py
+++ test_scanner.py
@@ -1,4 +1,10 @@
 from bluepy.btle import Scanner, DefaultDelegate
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect("broker.emqx.io", 1883, 60)
+
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -17,3 +23,5 @@ for dev in devices:
     print ("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
     for (adtype, desc, value) in dev.getScanData():
         print ("  %s = %s" % (desc, value))
+        a = [adtype,desc,value]
+        client.publish('raspberry/topic', payload=a, qos=0, retain=False)
